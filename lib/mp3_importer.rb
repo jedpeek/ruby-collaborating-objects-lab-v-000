b61428041a2d630550be1a90c319b00264b2c803
Jed Peek <jedpeek@yahoo.com>
2018-06-12 16:13:36 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,10 +6,14 @@ class MP3Importer
     @path = path
   end
 
+  #def files
+  #  @files = Dir.entries(@path)
+  #  @files.delete_if{|file| file.length < 4}
+  #end
+
   def files
-    @files = Dir.entries(@path)
-    @files.delete_if{|file| file.length < 4}
-  end
+   @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+ end
 
   def import
     files.each {|file| Song.new_by_filename(file)}
