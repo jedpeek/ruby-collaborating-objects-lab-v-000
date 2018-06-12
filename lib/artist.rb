fcd43cb9a94791665c384a11f4d60ee6f902c4f6
Jed Peek <jedpeek@yahoo.com>
2018-06-12 16:04:39 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,6 +5,7 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self
   end
 
   def add_song(song)
@@ -17,7 +18,7 @@ class Artist
   end
 
   def save
-    @@all << self
+
   end
 
   def self.find_or_create_by_name(name)
