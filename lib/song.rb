78c97a7208fcb61c8743743b542a55aac9ee8c2d
Jed Peek <jedpeek@yahoo.com>
2018-06-12 16:27:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,17 +7,14 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    artist = filename.split(" - ")[0]
-    name = filename.split(" - ")[1]
-    song = self.new(name)
-    song.artist_name = artist
-    song
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    binding.pry
-    artist.add_song(name)
+    artist.add_song(self)
   end
-
 end
