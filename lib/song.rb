c7f1e7382a7c9fc01c5c64881940c17a46da447f
Jed Peek <jedpeek@yahoo.com>
2018-06-12 16:28:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def self.new_by_filename(filename)
     @artist = filename.split(" - ")[0]
     @name = filename.split(" - ")[1]
-    song = self.new(name)
+    song = self.new(@name)
     song.artist_name = @artist
     song
   end
